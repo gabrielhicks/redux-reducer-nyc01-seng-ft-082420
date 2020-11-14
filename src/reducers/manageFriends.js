9922dd58ec48ff5b3bc2d938f7ebd9e031ae5663
gabrielhicks <gabrielhicks@mail.com>
2020-11-14 10:37:44 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,7 @@
 export function manageFriends(state = {friends: []}, action){
     switch(action.type) {
         case "ADD_FRIEND":
-            return ({...state, friends: [
-                ...state.friends, 
-                action.friend]})
+            return ({friends: [...state.friends, action.friend]})
             break
         case "REMOVE_FRIEND":
             const oldFriends = state.friends.filter(friend => friend.id !== action.id)
