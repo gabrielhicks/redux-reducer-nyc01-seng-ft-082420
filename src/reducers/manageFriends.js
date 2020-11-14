6b56da10c70a6013ae23510aea8f5f086eaf53fa
gabrielhicks <gabrielhicks@mail.com>
2020-11-14 10:37:02 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -6,12 +6,9 @@ export function manageFriends(state = {friends: []}, action){
                 action.friend]})
             break
         case "REMOVE_FRIEND":
-            const friendIndex = state.friends.findIndex(friend => friend.id === action.id)
+            const oldFriends = state.friends.filter(friend => friend.id !== action.id)
             return ({
-                ...state, friends: [
-                    ...state.friends.slice(0, friendIndex),
-                    ...state.friends.slice(friendIndex + 1)
-                ]
+                friends: oldFriends
             })
             break
         default:
